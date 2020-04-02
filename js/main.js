60f6e37367cff0b64d308190a68606bf03e365ce
Karolina Leczycka <karolina.leczycka93@gmail.com>
2020-04-02 14:38:39 +0200

    sprawdzenie liczby

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var n=201;
+
+function liczba(n){
+    return n;
+}
+if ((100<=n)&&(n<=200)){
+    console.log("Liczbna znajduje się w przedziale");
+} else {
+    console.log("Liczbna nie znajduje się w przedziale");
+}
